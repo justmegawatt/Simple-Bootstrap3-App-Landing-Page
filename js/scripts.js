ce7255af7ac3904c4ec227d9834e9c232b191dae
Megg Gawat <megawatt@live.com>
2018-03-23 19:53:10 -0400

    Finished top section, started on middle section

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,16 @@ function addPaddingToBody() {
 }
 addPaddingToBody();
 
-function resizeTopContainer() {
+function resizeSectionContainers() {
     let navbarHeight = $('.navbar').height();
     let windowHeight = $(window).height() - navbarHeight;
-    $('#top-container').css('height', windowHeight + 'px');
+    $('.section-container').css('height', windowHeight + 'px');
 }
-resizeTopContainer();
+resizeSectionContainers();
+
+// function moveTopContentDown() {
+//     let navbarHeight = $('.navbar').height();
+//     let windowHeight = $(window).height() - navbarHeight;
+//     $('#top-content').css('marginTop', windowHeight/4)
+// }
+// moveTopContentDown();
